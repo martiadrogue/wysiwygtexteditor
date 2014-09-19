6257e38228cca571a4da51ca1094f1fc2b14f030
martiadrogue <marti.adrogue@gmail.com>
2014-09-19 12:06:16 +0200

    Update main.js

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -56,3 +56,13 @@ function submit_form() {
   form.elements['body'].value = window.frames['wordProcessor'].document.body.innerHTML;
   form.submit();
 }
+wordProcessor.addEventListener("paste", function(e) {
+    // cancel paste
+    e.preventDefault();
+
+    // get text representation of clipboard
+    var text = e.clipboardData.getData("text/plain");
+
+    // insert text manually
+    document.execCommand("insertHTML", false, text);
+});
